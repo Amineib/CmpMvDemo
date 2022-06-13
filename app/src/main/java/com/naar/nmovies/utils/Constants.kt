8ede7dfcf2ca88746674b2ca03ae3f1a7ef3acdf
naar0x <mohamedamine.ib@gmail.com>
2022-06-13 17:02:45 +0200

    +test api keys

--- app/src/main/java/com/naar/nmovies/utils/Constants.kt
+++ app/src/main/java/com/naar/nmovies/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.naar.nmovies.utils
 
 object Constants {
-    const val API_KEY = "062e032a2b09ede68c1713f446f20d43" //NMOVIES KEY
+    const val API_KEY = "a48952ed90a177f26bb620a6fca58be9"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_IMG_URL = "https://image.tmdb.org/t/p/w500"
     const val BASE_YOUTUBE_URL = "https://youtube.com/watch?v="
